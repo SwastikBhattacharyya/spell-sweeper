c6e0572d6994b4ed6e62b72b71b64d08c9d83529
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-08-25 12:25:14 +0530

    test(edit-distance): add tests to verify Jaro and Jaro-Winkler distance
    Introduce two tests to verify jaro and jaro winkler distance for
    multiple pair of words.

--- src/test/edit_distance.cpp
+++ src/test/edit_distance.cpp
@@ -15,3 +15,17 @@ TEST(edit_distance_test, damerau_levenshtein) {
                   "Hello", "Hekko", 255),
               2);
 }
+
+TEST(edit_distance_test, jaro) {
+    EXPECT_GT(spell_sweeper::edit_distance::get_jaro("this", "htis"),
+              spell_sweeper::edit_distance::get_jaro("thus", "this"));
+    EXPECT_GT(spell_sweeper::edit_distance::get_jaro("pint", "piny"),
+              spell_sweeper::edit_distance::get_jaro("pent", "piny"));
+}
+
+TEST(edit_distance_test, jaro_winkler) {
+    EXPECT_GT(spell_sweeper::edit_distance::get_jaro_winkler("this", "thos"),
+              spell_sweeper::edit_distance::get_jaro("this", "thos"));
+    EXPECT_GT(spell_sweeper::edit_distance::get_jaro_winkler("neck", "necl"),
+              spell_sweeper::edit_distance::get_jaro_winkler("peck", "necl"));
+}
