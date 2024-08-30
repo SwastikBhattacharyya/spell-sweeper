22b30375b1e4de3cc1b5a56c2a9af8acb59bff52
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-08-30 13:19:23 +0530

    refactor(bloom-filter): add init function and default constructor

--- src/main/include/bloom_filter.h
+++ src/main/include/bloom_filter.h
@@ -7,6 +7,7 @@
 #include <string_view>
 #include <vector>
 
+namespace spell_sweeper {
 class bloom_filter {
   public:
     double fp_prob;
@@ -15,12 +16,15 @@ class bloom_filter {
     std::vector<uint8_t> bitarray;
 
   public:
+    bloom_filter() = default;
     bloom_filter(uint32_t items_count, double fp_prob);
+    void init(uint32_t items_count, double fp_prob);
     void insert(std::string_view target);
     bool lookup(std::string_view target);
 
     static size_t get_size(uint32_t items_count, double fp_prob);
     static uint32_t get_hash_count(uint32_t items_count, double fp_prob);
 };
+} // namespace spell_sweeper
 
 #endif
