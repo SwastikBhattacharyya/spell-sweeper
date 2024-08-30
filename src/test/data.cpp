5cd0bbe539cec7a9bfa95e059d7f3b3a42e13438
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-08-30 13:20:15 +0530

    test(data): add test to verify data class functions

---
+++ src/test/data.cpp
@@ -0,0 +1,33 @@
+#include "include/data.h"
+#include <gtest/gtest.h>
+
+TEST(data_test, get_dictionary) {
+    spell_sweeper::data app_data;
+    std::ifstream file("resources/words.txt");
+    ASSERT_NO_THROW(app_data.read_dictionary(file));
+    file.close();
+}
+
+TEST(data_test, init_bk_tree) {
+    spell_sweeper::data app_data;
+    std::ifstream file("resources/words.txt");
+    ASSERT_NO_THROW(app_data.read_dictionary(file));
+    file.close();
+
+    ASSERT_NO_THROW(app_data.init_bk_tree());
+
+    for (const auto& word : app_data.words)
+        EXPECT_EQ(app_data.tree.search(word), 0);
+}
+
+TEST(data_test, init_bloom_filter) {
+    spell_sweeper::data app_data;
+    std::ifstream file("resources/words.txt");
+    ASSERT_NO_THROW(app_data.read_dictionary(file));
+    file.close();
+
+    ASSERT_NO_THROW(app_data.init_bloom_filter());
+
+    for (const auto& word : app_data.words)
+        EXPECT_NE(app_data.filter.lookup(word), false);
+}
